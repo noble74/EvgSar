f1f2c190f9319ef7935d1f7bb19168ac7dd99c01
Noble74 <Noble74@192.168.8.100>
2015-01-08 19:26:01 +0600

    v0.1

---
+++ system/Core/Core.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Core
+ *
+ * @author Noble74
+ */
+class Core {
+    //put your code here
+}
